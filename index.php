3f0c5626a7ac1e24c6f677af18f00f01552442c7
Nazarov1987 <45045147+Nazarov1987@users.noreply.github.com>
2020-11-22 16:49:46 +0300

    Внесены изменения
    Реализовано подключение к БД. Запись в Redis. Посчитано время реализации запросов в БД и Redis.

--- index.php
+++ index.php
@@ -10,10 +10,41 @@ use Monolog\Handler\StreamHandler;
 $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
 
-if($redis->get('name')==false){
-$mysql = new mysqli('localhost', 'root', 'Altair1215!', 'db_vagrant');
-if($mysql->connect_errno) exit('Ошибка подключения к базе данных');
+if($redis->get('price')==false){
+$time_start = microtime();
+   $mysql = new mysqli('127.0.0.1', 'root', 'Altair1215!', 'db_vagrant');
+if ($mysql->connect_errno) {
+    echo "Не удалось подключиться к MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+else{echo 'Connected' ."<br>\n";}
+
+  $query = "SELECT productName,productDescription,buyPrice FROM products WHERE productCode = 'S24_2011'";
+   $result = mysqli_query($mysql,$query);
+   while($row = mysqli_fetch_array($result))
+   {
+echo "Вывод из БД" ."<br>\n";
+echo "Наименование товара: ".$row['productName']."<br>\n";
+echo "Описание: ".$row['productDescription']."<br>\n";
+echo "Цена: ".$row['buyPrice']."<br><hr>\n";
+$time_end = microtime();
+$time = $time_end - $time_start;
+echo 'Время выполнения БД: ' . $time;
+$redis->set('name', $row['productName']);
+$redis->set('desk', $row['productDescription']);
+$redis->set('price', $row['buyPrice']);
+   }
+}
+else{
+$time_start = microtime();
+echo "Вывод из Redis";
+var_dump($redis->get('name'));
+var_dump($redis->get('desk'));
+var_dump($redis->get('price'));
+$time_end = microtime();
+$time = $time_end - $time_start;
+echo 'Время выполнения Radis: ' . $time;
 };
+
 
 
 // create a log channel
